d9bf81c3d2b13e1b833349814dee3c0ee025185b
aleksi-svaridze <aleksi.svaridze08b@geolab.edu.ge>
2025-05-08 20:10:46 +0400

    Center settings section

--- src/components/ToogleTheme.tsx
+++ src/components/ToogleTheme.tsx
@@ -10,27 +10,30 @@ const ToogleTheme = ({ setDark, dark, showSettings }: ThemeType) => {
   return (
     <div
       style={{ backgroundColor: "rgba(0,0,0, .95)" }}
-      className={`w-full absolute top-0 bottom-0 right-0 left-0 flex flex-col gap-y-5 items-center pt-40 ${
+      className={`w-full absolute top-0 bottom-0 right-0 left-0 ${
         showSettings ? "visible" : "hidden"
       }`}
     >
-      <h3 className="text-white text-2xl">Settings</h3>
+      <div className="flex flex-col gap-y-5 items-center justify-center h-full">
+        <h3 className="text-white text-2xl">Settings</h3>
 
-      <button
-        className="text-white flex items-center gap-x-5 border-[1px] rounded-md border-white w-52 h-10 justify-center"
-        onClick={() => setDark((prev) => !prev)}
-      >
-        {dark ? (
-          <>
-            <BsSun /> Light
-          </>
-        ) : (
-          <>
-            <BsMoonStars /> Dark
-          </>
-        )}
-      </button>
-      <div className="border-[1px] rounded-md border-white w-52 h-20"></div>
+        <button
+          className="text-white flex items-center gap-x-5 border-[1px] rounded-md border-white w-52 h-10 justify-center"
+          onClick={() => setDark((prev) => !prev)}
+        >
+          {dark ? (
+            <>
+              <BsSun /> Light
+            </>
+          ) : (
+            <>
+              <BsMoonStars /> Dark
+            </>
+          )}
+        </button>
+
+        <div className="border-[1px] rounded-md border-white w-52 h-20"></div>
+      </div>
     </div>
   );
 };
